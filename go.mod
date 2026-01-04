31fb39cbad559d67085f570cdcfdbdf494f3c470
rahul-rj-battlebucks <stkwhy000@gmail.com>
2026-01-04 22:42:27 +0530

    configration done

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/RahulKumar9988/auth-microservices-goFiber
 
 go 1.22.2
+
+require github.com/gofiber/fiber/v2 v2.52.10
+
+require github.com/valyala/bytebufferpool v1.0.0 // indirect
---
+++ internal/config/config.go
@@ -0,0 +1,65 @@
+package config
+
+import (
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+type DBConfig struct {
+	URL            string
+	MaxOpenConns   int
+	MaxIdleConns   int
+	ConnMaxLife    time.Duration
+	ConnectTimeout time.Duration
+}
+
+type Config struct {
+	AppPort string
+	DB      DBConfig
+}
+
+func Load() *Config {
+	cfg := &Config{}
+
+	cfg.AppPort = getEnv("APP_PORT", "8080")
+
+	cfg.DB.URL = mustGetEnv("DB_URL")
+	cfg.DB.MaxOpenConns = getEnvInt("DB_MAX_OPEN", 25)
+	cfg.DB.MaxIdleConns = getEnvInt("DB_MAX_IDLE", 10)
+	cfg.DB.ConnMaxLife = time.Minute * 30
+	cfg.DB.ConnectTimeout = time.Second * 5
+
+	return cfg
+}
+
+func mustGetEnv(key string) string {
+	val := os.Getenv(key)
+
+	if val == "" {
+		log.Fatal("missing requied env", key)
+	}
+	return val
+}
+
+func getEnv(key, defaultVal string) string {
+	if val := os.Getenv(key); val != "" {
+		return val
+	}
+	return defaultVal
+}
+
+func getEnvInt(key string, defaultVal int) int {
+	valStr := os.Getenv(key)
+	if valStr == "" {
+		return defaultVal
+	}
+
+	val, err := strconv.Atoi(valStr)
+	if err != nil {
+		log.Fatalf("invalid int value for env %s: %v", key, err)
+	}
+
+	return val
+}
