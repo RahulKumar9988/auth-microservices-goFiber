0a6ac38450e1fe03dedc26da8da159ae92584202
rahul-rj-battlebucks <stkwhy000@gmail.com>
2026-01-05 14:11:31 +0530

    working on http server

--- internal/services/http.go
+++ internal/services/http.go
@@ -1,1 +1,35 @@
 package services
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func Start(app *fiber.App, port string) {
+
+	go func() {
+		if err := app.Listen(":" + port); err != nil {
+			log.Fatalf("server faield : %v", err)
+		}
+	}()
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT, syscall.SIGTERM)
+
+	<-shutdown
+	log.Println("shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := app.ShutdownWithContext(ctx); err != nil {
+		log.Printf("server shutdown error: %v", err)
+	}
+
+}
