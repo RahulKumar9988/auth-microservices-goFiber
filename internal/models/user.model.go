bc11408f836a049756bd77a6cd2e62deb560ed8c
rahul-rj-battlebucks <stkwhy000@gmail.com>
2026-01-07 11:21:00 +0530

    working on redis config

--- internal/models/user.model.go
+++ internal/models/user.model.go
@@ -10,10 +10,10 @@ const (
 )
 
 type UserModel struct {
-	ID         uint      `json:"id" gorm:"Primerykey"`
-	Email      string    `json:"email" gorm:"text;not null"`
-	Role       UserRole  `json:"role" gorm:"text;not null"`
-	Password   string    `json:"-"`
-	Created_At time.Time `json:"created_at"`
-	Updated_At time.Time `json:"updated_at"`
+	ID        uint      `json:"id" gorm:"primaryKey"`
+	Email     string    `json:"email" gorm:"uniqueIndex;not null"`
+	Role      UserRole  `json:"role" gorm:"type:text;not null;default:'user'"`
+	Password  string    `json:"-" gorm:"not null"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
